351de17d6be9fa1a444b6c8fee5c655eac5af059
Markus Blaschke <mblaschke82@gmail.com>
2021-02-20 22:37:03 +0100

    improve value type detection
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- process.go
+++ process.go
@@ -21,7 +21,31 @@ func convertSubMetricInterfaceToArray(val interface{}) []interface{} {
 func processFieldAndAddToMetric(fieldName string, value interface{}, fieldConfig config.ConfigQueryMetricField, metric *MetricRow) {
 	labelName := fieldConfig.GetTargetFieldName(fieldName)
 
+	// upgrade to 64bit
 	switch v := value.(type) {
+	// ----------------------------------------------------
+	// int
+	case int8:
+		value = int64(v)
+	case uint8:
+		value = int64(v)
+	case int16:
+		value = int64(v)
+	case uint16:
+		value = int64(v)
+	case int32:
+		value = int64(v)
+	case uint32:
+		value = int64(v)
+	// ----------------------------------------------------
+	// float
+	case float32:
+		value = float64(v)
+	}
+
+	switch v := value.(type) {
+	// ----------------------------------------------------
+	// string
 	case string:
 		v = fieldConfig.TransformString(v)
 		if fieldConfig.IsTypeValue() {
@@ -33,22 +57,35 @@ func processFieldAndAddToMetric(fieldName string, value interface{}, fieldConfig
 		} else {
 			metric.Labels[labelName] = v
 		}
-	case int64:
+	// ----------------------------------------------------
+	// int
+	case uint64:
 		fieldValue := fieldConfig.TransformFloat64(float64(v))
-
 		if fieldConfig.IsTypeValue() {
 			metric.Value = float64(v)
 		} else {
 			metric.Labels[labelName] = fieldValue
 		}
+	case int64:
+		fieldValue := fieldConfig.TransformFloat64(float64(v))
+		if fieldConfig.IsTypeValue() {
+			metric.Value = float64(v)
+		} else {
+			metric.Labels[labelName] = fieldValue
+		}
+
+	// ----------------------------------------------------
+	// float
 	case float64:
 		fieldValue := fieldConfig.TransformFloat64(v)
-
 		if fieldConfig.IsTypeValue() {
 			metric.Value = v
 		} else {
 			metric.Labels[labelName] = fieldValue
 		}
+
+	// ----------------------------------------------------
+	// boolean
 	case bool:
 		fieldValue := fieldConfig.TransformBool(v)
 		if fieldConfig.IsTypeValue() {
