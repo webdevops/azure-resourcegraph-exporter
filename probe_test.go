07f39ba8dcb0278f9925bc029595807ec339b7f7
Markus Blaschke <mblaschke82@gmail.com>
2021-02-21 13:17:40 +0100

    improve test readability
    Signed-off-by: Markus Blaschke <mblaschke82@gmail.com>

--- probe_test.go
+++ probe_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"github.com/webdevops/azure-resourcegraph-exporter/config"
+	"gopkg.in/yaml.v2"
 	"testing"
 )
 
@@ -26,44 +27,28 @@ type (
 )
 
 func TestMetricRowParsing(t *testing.T) {
-	result := `{
+	resultRow := parseResourceGraphJsonToResultRow(t, `{
 "name": "foobar",
 "count_": 20,
 "resources": 13,
 "should-not-exists": "testing"
-}`
+}`)
 
-	azureTestingFields := []config.ConfigQueryMetricField{
-		{
-			Name:   "name",
-			Target: "id",
-			Type:   "id",
-		},
-		{
-			Name: "count_",
-			Type: "value",
-		},
-		{
-			Name:   "resources",
-			Metric: "azure_testing_resources",
-			Type:   "value",
-		},
-	}
 
-	queryConfig := config.ConfigQuery{
-		Metric: "azure_testing",
-		MetricConfig: config.ConfigQueryMetric{
-			Fields: azureTestingFields,
-			DefaultField: config.ConfigQueryMetricField{
-				Type: "ignore",
-			},
-		},
-	}
-
-	resultRow := map[string]interface{}{}
-	if err := json.Unmarshal([]byte(result), &resultRow); err != nil {
-		t.Fatalf(`unable to unmarshal json: %v`, err)
-	}
+	queryConfig := parseMetricConfig(t, `
+metric: azure_testing
+fields:
+- name: name
+  target: id
+  type: id
+- name: count_
+  type: value
+- name: resources
+  metric: azure_testing_resources
+  type: value
+defaultField:
+  type: ignore
+`)
 
 	metricList := buildPrometheusMetricList(queryConfig.Metric, queryConfig.MetricConfig, resultRow)
 
@@ -89,70 +74,38 @@ func TestMetricRowParsing(t *testing.T) {
 }
 
 func TestMetricRowParsingWithFilters(t *testing.T) {
-	result := `{
+	resultRow := parseResourceGraphJsonToResultRow(t, `{
 "name": "/subscription/xxxxXx-xxxxx-xxxxx-xxxxx/resourceGroup/zzzzzzzzzzzz/providerid/resourcename",
 "created": "2021-01-20 12:23:34",
 "invalid": "0",
 "valid": "yes"
-}`
+}`)
 
-	azureTestingFields := []config.ConfigQueryMetricField{
-		{
-			Name:   "name",
-			Target: "id",
-			Type:   "id",
-		},
-		{
-			Name:   "name",
-			Target: "subscription",
-			Filters: []config.ConfigQueryMetricFieldFilter{
-				{
-					Type: "tolower",
-				},
-				{
-					Type:        "regexp",
-					RegExp:      "/subscription/([^/]+)/.*",
-					Replacement: "$1",
-				},
-			},
-		},
-		{
-			Name: "created",
-			Filters: []config.ConfigQueryMetricFieldFilter{
-				{Type: "tounixtime"},
-			},
-		},
-		{
-			Name: "created",
-			Type: "value",
-			Filters: []config.ConfigQueryMetricFieldFilter{
-				{Type: "tounixtime"},
-			},
-		},
-		{
-			Name: "invalid",
-			Type: "bool",
-		},
-		{
-			Name: "valid",
-			Type: "bool",
-		},
-	}
-
-	queryConfig := config.ConfigQuery{
-		Metric: "formatter-test",
-		MetricConfig: config.ConfigQueryMetric{
-			Fields: azureTestingFields,
-			DefaultField: config.ConfigQueryMetricField{
-				Type: "ignore",
-			},
-		},
-	}
-
-	resultRow := map[string]interface{}{}
-	if err := json.Unmarshal([]byte(result), &resultRow); err != nil {
-		t.Fatalf(`unable to unmarshal json: %v`, err)
-	}
+	queryConfig := parseMetricConfig(t, `
+metric: formatter-test
+fields:
+- name: name
+  target: id
+  type: id
+- name: name
+  target: subscription
+  filters:
+  - type: tolower
+  - type: regexp
+    regexp: /subscription/([^/]+)/.*
+    replacement: $1
+- name: created
+  filters: [toUnixtime]
+- name: created
+  type: value
+  filters: [toUnixtime]
+- name: invalid
+  type: bool
+- name: valid
+  type: bool
+defaultField:
+  type: ignore
+`)
 
 	metricList := buildPrometheusMetricList(queryConfig.Metric, queryConfig.MetricConfig, resultRow)
 
@@ -169,52 +122,79 @@ func TestMetricRowParsingWithFilters(t *testing.T) {
 	metricTestSuite.metric("formatter-test").row(0).assertValue(1611145414)
 }
 
+func parseResourceGraphJsonToResultRow(t *testing.T, data string) map[string]interface{} {
+	t.Helper()
+	ret := map[string]interface{}{}
+	if err := json.Unmarshal([]byte(data), &ret); err != nil {
+		t.Fatalf(`unable to unmarshal json: %v`, err)
+	}
+	return ret
+}
+
+func parseMetricConfig(t *testing.T, data string) config.ConfigQuery {
+	t.Helper()
+	ret := config.ConfigQuery{}
+	if err := yaml.Unmarshal([]byte(data), &ret); err != nil {
+		t.Fatalf(`unable to unmarshal json: %v`, err)
+	}
+	return ret
+}
 
 func (m *testingMetricResult) assertMetricCount(count int) {
+	m.t.Helper()
 	if val := len(m.list); val != count {
 		m.t.Fatalf(`metric count not valid, expected: %v, found: %v`, count, val)
 	}
 }
 
 func (m *testingMetricResult) assertMetric(name string) {
+	m.t.Helper()
 	if _, exists := m.list[name]; !exists {
 		m.t.Fatalf(`expected metric "%v" not found`, name)
 	}
 }
 
 func (m *testingMetricResult) metric(name string) *testingMetricList {
+	m.t.Helper()
 	return &testingMetricList{t: m.t, list: m.list[name], name: name}
 }
 
 func (m *testingMetricList) assertRowCount(count int) {
+	m.t.Helper()
 	if val := len(m.list); val != count {
 		m.t.Fatalf(`metric row count for "%v" not valid, expected: %v, found: %v`, m.name, count, val)
 	}
 }
 
 func (m *testingMetricList) row(row int) *testingMetricRow {
+	m.t.Helper()
+
 	return &testingMetricRow{t: m.t, row: m.list[row], name: m.name}
 }
 
 func (m *testingMetricRow) assertLabelCount(count int) {
+	m.t.Helper()
 	if val := len(m.row.Labels); val != count {
 		m.t.Fatalf(`metric row "%v" has wrong label count; expected: "%v", got: "%v"`, m.name, count, val)
 	}
 }
 
 func (m *testingMetricRow) assertLabelNotExists(name string) {
+	m.t.Helper()
 	if _, exists := m.row.Labels[name]; exists {
 		m.t.Fatalf(`metric row "%v" has wrong "%v" label, should not exists`, m.name, name)
 	}
 }
 
 func (m *testingMetricRow) assertLabelExists(labelName string) {
+	m.t.Helper()
 	if _, exists := m.row.Labels[labelName]; !exists {
 		m.t.Fatalf(`metric row "%v" has wrong "%v" label, should exists`, m.name, labelName)
 	}
 }
 
 func (m *testingMetricRow) assertLabel(labelName, labelValue string) {
+	m.t.Helper()
 	if _, exists := m.row.Labels[labelName]; !exists {
 		m.t.Fatalf(`metric row "%v" has wrong "%v" label, should exists`, m.name, labelName)
 	}
@@ -225,6 +205,7 @@ func (m *testingMetricRow) assertLabel(labelName, labelValue string) {
 }
 
 func (m *testingMetricRow) assertValue(metricValue float64) {
+	m.t.Helper()
 	if val := m.row.Value; val != metricValue {
 		m.t.Fatalf(`metric row "%v" has wrong metric value; expected: "%v", got: "%v"`, m.name, metricValue, val)
 	}
